2990db40491be3bdc3c34b786d5c3fefe66fd72b
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-05 10:01:17 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -185,11 +185,14 @@ class FreeplayState extends MusicBeatState
 		text.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, RIGHT);
 		text.scrollFactor.set();
 		add(text);
+		addTouchPad("LEFT_FULL", "A_B_C_X_Y_Z");
 		super.create();
 	}
 
 	override function closeSubState() {
 		changeSelection(0, false);
+		removeTouchPad();
+		addTouchPad("LEFT_FULL", "A_B_C_X_Y_Z");
 		super.closeSubState();
 	}
 
@@ -246,11 +249,11 @@ class FreeplayState extends MusicBeatState
 		var upP = controls.UI_UP_P;
 		var downP = controls.UI_DOWN_P;
 		var accepted = controls.ACCEPT;
-		var space = FlxG.keys.justPressed.SPACE;
-		var ctrl = FlxG.keys.justPressed.CONTROL;
+		var space = FlxG.keys.justPressed.SPACE || touchPad.buttonX.justPressed;
+		var ctrl = FlxG.keys.justPressed.CONTROL || touchPad.buttonC.justPressed;
 
 		var shiftMult:Int = 1;
-		if(FlxG.keys.pressed.SHIFT) shiftMult = 3;
+		if(FlxG.keys.pressed.SHIFT || touchPad.buttonZ.pressed) shiftMult = 3;
 
 		if (upP)
 		{
@@ -278,6 +281,7 @@ class FreeplayState extends MusicBeatState
 
 		if(ctrl)
 		{
+			removeTouchPad();
 			openSubState(new GameplayChangersSubstate());
 		}
 		else if(space)
@@ -340,8 +344,9 @@ class FreeplayState extends MusicBeatState
 					
 			destroyFreeplayVocals();
 		}
-		else if(controls.RESET)
+		else if(controls.RESET || touchPad.buttonY.justPressed)
 		{
+			removeTouchPad();
 			openSubState(new ResetScoreSubState(songs[curSelected].songName, curDifficulty, songs[curSelected].songCharacter));
 			FlxG.sound.play(Paths.sound('scrollMenu'));
 		}
